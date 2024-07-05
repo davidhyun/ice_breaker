4ff715dbf09a439dafd520e98840dd1e74ee1257
davidhyun <hyunh317@gmail.com>
2024-07-06 00:36:39 +0900

    Add agent

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,10 +3,12 @@ from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 
-if __name__ == '__main__':
-    load_dotenv() # load environment variables dynamically
+def ice_break_with(name: str) -> str:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
     
     summary_template = """
     given the LinkedIn information {information} about a person I want you to create:
@@ -19,12 +21,13 @@ if __name__ == '__main__':
     )
     
     llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
-    
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url = "https://www.linkedin.com/in/hyunh317/"
-    )
     
     res = chain.invoke(input={"information": linkedin_data})
-    
-    print(res['text'])
+
+    return res['text']
+
+
+if __name__ == '__main__':
+    load_dotenv() # load environment variables dynamically
+    print(ice_break_with(name="Hyunho Choi"))
